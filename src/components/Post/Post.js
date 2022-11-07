5a16d7ca3398b1feb1c90d97f93cbecaa0e75933
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-11-07 23:21:44 +0000

    UPDATE: structure for post component so static view looks good

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,27 +1,59 @@
 import React from 'react';
 import './Post.css';
+import InputOption from '../InputOption/InputOption';
 import { Avatar } from '@mui/material';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import CommentIcon from '@mui/icons-material/Comment';
+import ShareIcon from '@mui/icons-material/Share';
+import SendIcon from '@mui/icons-material/Send';
 
 const Post = ({ name, description, message, photoUrl }) => {
   return (
     <div className='post'>
-      <div className="post__header">
+      <div className='post__header'>
         <Avatar />
 
-        <div className="post__info">
+        <div className='post__info'>
           <h2>
             Ronny Rahman
           </h2>
+
           <p>
             This is a test
           </p>
         </div>
+      </div>
 
-        <div className="post__body">
-          <p>
-            This worked ... yes
-          </p>
-        </div>
+      <div className='post__body'>
+        <p>
+          This worked ... yes
+        </p>
+      </div>
+
+      <div className='post__buttons'>
+        <InputOption
+          title='Like'
+          Icon={ThumbUpIcon}
+          color='686a6c'
+        />
+
+        <InputOption
+          title='Comment'
+          Icon={CommentIcon}
+          color='686a6c'
+        />
+
+        <InputOption
+          title='Share'
+          Icon={ShareIcon}
+          color='686a6c'
+        />
+
+        <InputOption
+          title='Send'
+          Icon={SendIcon}
+          color='686a6c'
+        />
       </div>
     </div>
   )
