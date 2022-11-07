c0e9c2e6530dc109b5d004c7c2e51efda9473b78
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-11-07 21:27:01 +0000

    UPDATE: styling of header left side of component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,6 @@ import SearchIcon from '@mui/icons-material/Search';
 const Header = () => {
   return (
     <div className='header'>
-      <h1>header component</h1>
-
       <div className='header__left'>
         <img
           src='https://img.icons8.com/external-justicon-flat-justicon/100/000000/external-linkedin-social-media-justicon-flat-justicon.png'
