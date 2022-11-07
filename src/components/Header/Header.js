6dc9cd94d79920198d72fd1f0287aa717a9d9929
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-11-07 21:40:53 +0000

    ADD: header option component to reuse on header nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import HeaderOption from '../HeaderOption/HeaderOption';
 
 const Header = () => {
   return (
@@ -18,7 +24,35 @@ const Header = () => {
       </div>
 
       <div className='header__right'>
+        <HeaderOption
+          Icon={HomeIcon}
+          title='Home'
+        />
 
+        <HeaderOption
+          Icon={SupervisorAccountIcon}
+          title='My Network'
+        />
+
+        <HeaderOption
+          Icon={BusinessCenterIcon}
+          title='Jobs'
+        />
+
+        <HeaderOption
+          Icon={ChatIcon}
+          title='Messages'
+        />
+
+        <HeaderOption
+          Icon={NotificationsIcon}
+          title='Notifications'
+        />
+
+        <HeaderOption
+          avatar='https://media-exp1.licdn.com/dms/image/C4D03AQGRdw2cZmz0FA/profile-displayphoto-shrink_100_100/0/1625271689587?e=1673481600&v=beta&t=oBBASaE4qtiOTg4Ctqndizl8RkEi0LR1Fapv9zEbUrk'
+          title='Ronny'
+        />
       </div>
     </div>
   )
