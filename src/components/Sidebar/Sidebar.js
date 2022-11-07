29b83e6f21bd82327b998d48aba1521f3e61a0c3
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-11-07 22:13:15 +0000

    ADD: sidebar component markup

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Sidebar.css';
+import { Avatar } from '@mui/material';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="sidebar__top">
+        <img
+          src='https://i.pinimg.com/736x/dc/55/c2/dc55c27ccd37988dd63e5db558dca3d7.jpg' alt='Background Banner'
+        />
+        <Avatar
+          className='sidebar__avatar'
+        />
+        <h2>ronny rahman</h2>
+        <h4>rejaur.ronny.rahman@gmail.com</h4>
+      </div>
+
+      <div className='sidebar__stats'>
+        <div className='sidebar__stat'>
+          <p>who viewed you</p>
+          <p className='sidebar__statNumber'>2,554</p>
+        </div>
+        <div className='sidebar__stat'>
+          <p>views on post</p>
+          <p className='sidebar__statNumber'>2,236</p>
+        </div>
+      </div>
+
+      <div className='sidebar__bottom'>
+        <h4>recent</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar;
