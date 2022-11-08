c490e6000e2cfb2cfeb53cb4b36879acb1d3eaf9
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-11-08 15:44:50 +0000

    ADD: authentication to login screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Login from './components/Login/Login';
-import { selectUser } from './features/userSlice';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './services/firebase';
 
 const App = () => {
   const user =useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoURL: userAuth.photoURL
+        }))
+      } else {
+        dispatch(logout());
+      }
+    })
+  }, [])
 
   return (
     <div className='app'>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import { auth } from '../../services/firebase';
 import LinkedinLogo from '../../assets/linkedin-logo.png';
+import { useDispatch } from 'react-redux';
+import { login } from '../../features/userSlice';
 
 const Login = () => {
-  const loginToApp = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [profilePic, setProfilePic] = useState('');
+  const dispatch = useDispatch();
 
+  const loginToApp = (e) => {
+    e.preventDefault();
   }
 
   const register = () => {
+    if (!name) {
+      return alert('Please enter a full name!');
+    }
 
+    auth.createUserWithEmailAndPassword(email, password).then((userAuth) => {
+      userAuth.user.updateProfile({
+        displayName: name,
+        photoURL: profilePic
+      })
+      .then(() => {
+        dispatch(login({
+          email: userAuth.user.email,
+          uid: userAuth.user.uid,
+          displayName: name,
+          photoURL: profilePic
+        }))
+      })
+    }).catch(error => alert(error))
   }
 
   return (
@@ -19,11 +45,40 @@ const Login = () => {
       />
 
       <form>
-        <input placeholder='Full name (required if registering)' type='text' />
-        <input placeholder='Profile pic URL (optional)' type='text' />
-        <input placeholder='Email address' type='email' />
-        <input placeholder='Password' type='password' />
-        <button type='submit' onClick={loginToApp}>sign in</button>
+        <input
+          value={name}
+          placeholder='Full name (required if registering)'
+          type='text'
+          onChange={e => setName(e.target.value)}
+        />
+
+        <input
+          value={profilePic}
+          placeholder='Profile pic URL (optional)'
+          type='text'
+          onChange={e => setProfilePic(e.target.value)}
+        />
+
+        <input
+          value={email}
+          placeholder='Email address'
+          type='email'
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <input
+          value={password}
+          placeholder='Password'
+          type='password'
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <button
+          type='submit'
+          onClick={loginToApp}
+        >
+          sign in
+        </button>
       </form>
 
       <p>Not a member? <span className='login__register' onClick={register}>register now</span></p>
